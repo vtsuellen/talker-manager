807eb1d5c546940b36bdbecf22d437578c25ff4d
vtsuellen <vtsuellen@hotmail.com>
2023-10-08 22:54:22 -0300

    feat: Crie o endpoint POST /login

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { readFiles, getTalkerById } = require('../utils/fs');
+const { readFiles, getTalkerById, generateToken } = require('../utils/fs');
 
 const router = Router();
 
@@ -13,6 +13,12 @@ router.get('/talker/:id', async (_req, res) => {
   const talkers = await getTalkerById(Number(id));
   if (!talkers) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   return res.status(200).json(talkers); 
+});
+
+router.post('/login', async (_req, res) => {
+  const token = generateToken(16);
+  if (!token) return res.status(401).json({ message: 'Email e senha são obrigatórios' });
+  return res.status(200).json({ token });
 });
 
 module.exports = router;
--- src/utils/fs.js
+++ src/utils/fs.js
@@ -1,4 +1,5 @@
 const fs = require('fs').promises;
+const crypto = require('crypto');
 
 const readFiles = async () => {
   const data = await fs.readFile('src/talker.json', 'utf-8');
@@ -11,7 +12,16 @@ const getTalkerById = async (id) => {
   return newData;
 };
 
+const generateToken = (length) => {
+  const byteLength = Math.ceil(length / 2);
+  const buffer = crypto.randomBytes(byteLength);
+  const token = buffer.toString('hex').slice(0, length);
+
+  return token;
+};
+
 module.exports = {
   readFiles,
   getTalkerById,
+  generateToken,
 };
