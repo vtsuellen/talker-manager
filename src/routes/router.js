737136511aeed50aa76801d26e4739831765e62a
vtsuellen <vtsuellen@hotmail.com>
2023-10-10 01:20:35 -0300

    refactor: add statusCode

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const statusCode = require('../utils/statusCode');
 const { readFiles, getTalkerById, generateToken, getTalkers, addTalker } = require('../utils/fs');
 const { validateLogin } = require('../middlewares/validateLogin');
 const { validateAge } = require('../middlewares/validateAge');
@@ -12,20 +13,27 @@ const router = Router();
 
 router.get('/talker', async (_req, res) => {  
   const talkers = await readFiles();
-  return res.status(200).json(talkers);
+  return res.status(statusCode.OK).json(talkers);
 });
 
 router.get('/talker/:id', async (_req, res) => {
   const { id } = _req.params;
   const talkers = await getTalkerById(Number(id));
-  if (!talkers) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
-  return res.status(200).json(talkers); 
+  if (!talkers) {
+    return res.status(statusCode.NOT_FOUND)
+      .json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(statusCode.OK).json(talkers); 
 });
 
 router.post('/login', validateLogin, async (_req, res) => {
-  const token = generateToken(16);
-  if (!token) return res.status(401).json({ message: 'Email e senha são obrigatórios' });
-  return res.status(200).json({ token });
+  try {
+    const token = generateToken(16);
+    return res.status(statusCode.OK).json({ token });
+  } catch (error) {
+    console.log(error);
+    res.status(statusCode.INTERNAL_SERVER_ERROR).json({ message: error });
+  }
 });
 
 router.post('/talker',
@@ -42,10 +50,10 @@ router.post('/talker',
       talkers.push(newTalker);
       await addTalker(talkers);
       console.log(newTalker);
-      return res.status(201).json(newTalker);
+      return res.status(statusCode.CREATED).json(newTalker);
     } catch (error) {
       console.log(error);
-      res.status(500).json({ message: error });
+      res.status(statusCode.INTERNAL_SERVER_ERROR).json({ message: error });
     }
   });
 
---
+++ src/utils/statusCode.js
@@ -0,0 +1,12 @@
+const statusCode = {
+  OK: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  SERVICE_UNAVAILABLE: 503,
+  GATEWAY_TIMEOUT: 504,
+};
+
+module.exports = statusCode;
